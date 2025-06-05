46f6a5e049e21c13f5581799c2784f685ea3c378
V1N78 <159727923+V1N78@users.noreply.github.com>
2025-06-05 12:26:04 +1200

    update

---
+++ milestone 3/Milestone2.py
@@ -0,0 +1,81 @@
+#intro
+print("Welcome to my quiz!")
+print("----------------------------------------")
+print("In this quiz, you have to answer questions.. Goodluck!")
+print("----------------------------------------")
+#question 1
+user_answer = input("What is the tallest building?")
+A1 = "The Burj Khalifa"
+A1 = "Burj Khalifa"
+A1 = "the burj khalifa"
+A1 = "burj khalifa"
+print("Your answer is: " + user_answer)
+if user_answer == A1:
+    print("Correct! The Burj Khalifa is the tallest building at 828m, located in Dubai.")
+    print("----------------------------------------")
+else:
+    print("Incorrect! Better luck next time")
+    print("----------------------------------------")
+
+#question 2
+user_answer = input("Which country is both in Europe and Asia?")
+A1 = "Kazakhstan"
+A1 = "kazakhstan"
+print("Your answer is: " + user_answer)
+if user_answer == A1:
+    print("Correct!")
+    print("----------------------------------------")
+else:
+    print("Incorrect! Better luck next time")
+    print("----------------------------------------")
+
+#question 3
+user_answer = input("What is the capital of Brazil?")
+A1 = "Brasil"
+A1 = "brasil"
+print("Your answer is: " + user_answer)
+if user_answer == A1:
+    print("Correct!")
+    print("----------------------------------------")
+else:
+    print("Incorrect! Better luck next time")
+    print("----------------------------------------")
+
+#question 4
+user_answer = input("Which U.S. state is farthest west?")
+A1 = "Alaska"
+A1 = "alaska"
+print("Your answer is: " + user_answer)
+if user_answer == A1:
+    print("Correct! Alaska crosses the 180° meridian")
+    print("----------------------------------------")
+else:
+    print("Incorrect! Better luck next time")
+    print("----------------------------------------")
+
+#question 5
+user_answer = input("Which country has the most natural lakes?")
+A1 = "Canada"
+A1 = "canada"
+print("Your answer is: " + user_answer)
+if user_answer == A1:
+    print("Correct!")
+    print("----------------------------------------")
+else:
+    print("Incorrect! Better luck next time")
+    print("----------------------------------------")
+
+#question 6
+user_answer = input("Which country has the most time zones?")
+A1 = "France"
+A1 = "france"
+print("Your answer is: " + user_answer)
+if user_answer == A1:
+    print("Correct! It is due to overseas territories.")
+    print("----------------------------------------")
+else:
+    print("Incorrect! Better luck next time")
+    print("----------------------------------------")
+
+#ending
+print("Thank you for participating in the quiz!")
